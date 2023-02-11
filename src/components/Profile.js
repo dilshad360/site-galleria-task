163a706f0c1d6544b595dd2c2831900ee873251c
dilshad360 <dilshad360.dev@gmail.com>
2023-02-11 10:54:41 +0530

    fix: profile image

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,10 @@
+import ProfileImage from "../assets/profile.jpg";
+import Image from "next/image";
+
 export default function Profile() {
     return (
         <div className="border-2 p-4 rounded-2xl flex">
-            <img className="rounded-full w-16 mx-1" src="https://media.licdn.com/dms/image/C5603AQErHLDypRxt2Q/profile-displayphoto-shrink_400_400/0/1663251419452?e=1681344000&v=beta&t=mWclUjhadbIvjAfG42vUsa5rpidW8qDj6AOeH903ha4" alt="" />
+            <Image className="rounded-full w-16 mx-1" src={ProfileImage} alt="profile_image" />
             <div className="flex flex-col justify-center text-neutral-600 ml-2">
                 <h3>Dilshad</h3>
                 <h3>21212002</h3>
